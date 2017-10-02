fa0453a55fd519fbe52c41cf18d778671c8dcdcb
birdiedev@gmail.com <birdie@Birdies-MacBook-Pro.local>
2017-10-02 14:57:47 -0700

    added images array to js and connected to html

---
+++ app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var imgArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
+
+console.log(imgArray);
